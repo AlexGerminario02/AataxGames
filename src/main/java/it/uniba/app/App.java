d5a3a32abe10c2ddded6059dba7ddb323d8297a1
Nico576 <nico.studio78@gmail.com>
2024-05-13 17:47:33 +0200

    Modificato il metodo main della classe App

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,4 +1,5 @@
 package it.uniba.app;
+
 import java.util.Scanner;
 
 /**
@@ -21,6 +22,32 @@ public final class App {
      * @param args command line arguments
      */
     public static void main(final String[] args) {
-        System.out.println(new App().getGreeting());
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println(new App().getGreeting());
+            System.out.println("\nDigita '/help','--help' o '-h' per visulizzare l'elenco dei comandi ");
+            System.out.println("\n============================");
+
+            System.out.print("\nDigita un comando: ");
+            String input = scanner.nextLine().trim(); // inserisco il trim per rimuovere eventuali spazi bianchi
+
+            switch (input.toLowerCase()) {
+                case "/help":
+                case "--help":
+                case "-h":
+                    Menu.help(scanner);
+                    break;
+                case "/esci":
+                    isRunning = false; // esco dal ciclo while
+                    break;
+                default:
+                    System.out.println("Scelta non valida. Premi un pulsante valido\n");
+
+            }
+        }
+        scanner.close();
+
     }
 }

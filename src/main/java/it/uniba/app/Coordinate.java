e2b7a640e8e67e6b4652f9440cf05712ec6709b7
Sophia <andrisanosophia5@gmail.com>
2024-05-15 19:53:31 +0200

    Inizializzata la classe per gestire le coordinate.

---
+++ src/main/java/it/uniba/app/Coordinate.java
@@ -0,0 +1,28 @@
+package it.uniba.app;
+
+/**
+ * Classe per rappresentare le coordinate di una pedina sul tavoliere.
+ */
+public class Coordinate {
+    private int riga;
+    private char colonna;
+
+/**
+ * Costruttore della classe Coordinate.
+ * @param row
+ * @param column
+ */
+
+    public Coordinate(final int row, final char column) {
+        this.riga = row;
+        this.colonna = column;
+    }
+
+    public final char getColonna() {
+        return colonna;
+    }
+
+    public final int getRiga() {
+        return riga;
+    }
+}

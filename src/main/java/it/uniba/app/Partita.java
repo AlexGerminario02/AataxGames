6b5257e240ed5ced4f86beaaf82127f23b4202df
Sophia <andrisanosophia5@gmail.com>
2024-05-15 20:15:08 +0200

    Creata la predisposizione all'avvio della partita.

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
  */
 public class Partita {
 
+    private static final String MESSAGGIO_GIOCO = "Inserisci coordinate o comandi del menu: ";
+
     @SuppressWarnings("unused")
     private Giocatore giocatore2;
     @SuppressWarnings("unused")
     private Giocatore giocatore1;
-    @SuppressWarnings("unused")
     private Tavoliere tavoliere;
-    @SuppressWarnings("unused")
     private Scanner scanner;
 
     /**
@@ -25,5 +25,56 @@ public class Partita {
     public Partita(final Giocatore giocatoret1, final Giocatore giocatoret2, final Tavoliere tavolieret) {
         this.tavoliere = tavolieret;
         this.scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Funzione che Avvia la partita.
+     */
+
+    public void avviaPartita() {
+        tavoliere.visualizzaTavolierePieno();
+        while (!partitaFinita()) {
+            System.out.print(MESSAGGIO_GIOCO);
+            String input = scanner.nextLine().trim();
+            if (input.startsWith("/") || input.startsWith("-") || input.startsWith("--")) {
+                // Gestisci i comandi del menu
+                gestisciComando(input);
+            } else {
+                // Gestisci le coordinate inserite dall'utente
+                gestisciCoordinate(input);
+            }
+
+        }
+
+        scanner.close();
+    }
+
+    private boolean partitaFinita() {
+        // Implementa la logica per determinare se la partita è finita
+        return false;
+    }
+
+    /**
+     * Funzione che gestisce i comandi del menu.
+     * @param comando
+     */
+    private void gestisciComando(final String comando) {
+        // Implementa la logica per gestire i comandi del menu
+        switch (comando.toLowerCase()) {
+            case "/help":
+            case "-h":
+            case "--help":
+                Menu.help(scanner);
+                break;
+            default:
+                System.out.println("Comando non valido. Riprova.");
+        }
+    }
+
+    private void gestisciCoordinate(final String coordinate) {
+        // Implementa la logica per gestire le coordinate inserite dall'utente
+        System.out.println("Funzione da implementare per gestire le coordinate: " + coordinate);
+        System.out.println("");
+
     }
 }

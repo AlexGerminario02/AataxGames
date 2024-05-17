5b841658f548e473050bd37d0b773a0703e2d8de
pinopostino <cannonegiuseppe13@gmail.com>
2024-05-17 13:06:22 +0200

    Aggiunta funzione per la stampa delle mosse e della inzializzazione delle pedine

--- src/main/java/it/uniba/app/Tavoliere.java
+++ src/main/java/it/uniba/app/Tavoliere.java
@@ -17,8 +17,15 @@ public class Tavoliere {
     public static final int MOVE_DOWN_LEFT = 1;
     public static final int MOVE_LEFT_DOWN = -1;
     public static final int MOVE_LEFT_UP = -2;
+    public static final int RIGAINIZIALE = 1;
+    public static final int COLONNAINIZIALE = 1;
+    public static final int RIGAFINALE = 7;
+    public static final int COLONNAFINALE = 7;
     private static final String LINE_SEPARATOR = "+-----";
     private static final String EMPTY_CELL = "|  .  ";
+    private static final String ANSI_YELLOW = "\u001B[38;2;255;255;0m";
+    private static final String ANSI_ORANGE = "\u001B[38;2;255;165;0m";
+
 
     // Costanti per i caratteri dei giocatori
     private static final char PLAYER_X_CHAR = '\u26C0'; // 'X'
@@ -174,22 +181,21 @@ public class Tavoliere {
         System.out.println();
     }
 
-    /**
-     * Visualizza il tavoliere vuoto con le coordinate sulle righe e sui numeri.
-     */
-    public final void inizializzaPedine() {
-        int i = 0;
-        // Posiziona una pedina 'X' nella prima riga e nella prima colonna
-        setPedina(new Pedina('X', i + 1, i + 1), i + 1, colonne[0]);
-        // Posiziona una pedina 'O' nella prima riga e nell'ultima colonna
-        setPedina(new Pedina('O', i + 1, DIM - i), i + 1, colonne[DIM - 1]);
-        int j = DIM;
-        // Posiziona una pedina 'X' nell'ultima riga e nell'ultima colonna
-        setPedina(new Pedina('X', j, DIM - i), DIM, colonne[DIM - 1]);
-        // Posiziona una pedina 'O' nell'ultima riga e nella prima colonna
-        setPedina(new Pedina('O', DIM, i + 1), DIM, colonne[0]);
+  /**
+ * Visualizza il tavoliere vuoto con le coordinate sulle righe e sui numeri.
+ */
+public void inizializzaPedine(final int rigaIniziale, final int colonnaIniziale, final int rigaFinale,
+final int colonnaFinale) {
+    // Posiziona una pedina 'X' nella prima riga e nella prima colonna
+    setPedina(new Pedina('@', rigaIniziale, colonnaIniziale), rigaIniziale, colonne[0]);
+    // Posiziona una pedina 'O' nella prima riga e nell'ultima colonna
+    setPedina(new Pedina('$', rigaIniziale, colonnaFinale), rigaIniziale, colonne[DIM - 1]);
+    // Posiziona una pedina 'X' nell'ultima riga e nell'ultima colonna
+    setPedina(new Pedina('@', rigaFinale, colonnaFinale), rigaFinale, colonne[DIM - 1]);
+    // Posiziona una pedina 'O' nell'ultima riga e nella prima colonna
+    setPedina(new Pedina('$', rigaFinale, colonnaIniziale), rigaFinale, colonne[0]);
+}
 
-    }
 
     /**
      * Restituice il numero di mosse di tipo A disponibili nel gioco.
@@ -255,5 +261,85 @@ public class Tavoliere {
         mosseC.addAll(mosseB(riga, colonna));
         return mosseC;
     }
+   /**
+ * Stampa le mosse disponibili sul tavoliere.
+ *
+ * @param mosseA Lista delle mosse disponibili di tipo A.
+ * @param mosseB Lista delle mosse disponibili di tipo B.
+ */
+public void stampaMosseDisponibili(final ArrayList<Coordinate> mosseA, final ArrayList<Coordinate> mosseB) {
+    // Inizializza le pedine del tavoliere
+    inizializzaPedine(RIGAINIZIALE, COLONNAINIZIALE, RIGAFINALE, COLONNAFINALE);
+    Pedina[][] tabelloneStampato = new Pedina[DIM][DIM];
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM; j++) {
+            if (scacchiera[i][j] != null) {
+                tabelloneStampato[i][j] = new Pedina(scacchiera[i][j].getCarattere(), i, j);
+            }
+        }
+    }
+
+    // Segna le mosse disponibili di tipo A sul tavoliere
+    for (Coordinate mossa : mosseA) {
+        int riga = mossa.getRiga();
+        char colonna = mossa.getColonna();
+        tabelloneStampato[riga - 1][colonna - 'a'] = new Pedina('A', riga, colonna);
+    }
+
+    // Segna le mosse disponibili di tipo B sul tavoliere
+    for (Coordinate mossa : mosseB) {
+        int riga = mossa.getRiga();
+        char colonna = mossa.getColonna();
+        tabelloneStampato[riga - 1][colonna - 'a'] = new Pedina('B', riga, colonna);
+    }
+    // Stampa l'intestazione delle colonne
+    System.out.print("    ");
+    for (char colonna : colonne) {
+        System.out.print("   " + colonna + "  ");
+    }
+    System.out.println();
+
+    // Stampa il tavoliere con le mosse disponibili
+    for (int i = 1; i <= DIM; i++) {
+        System.out.print("  ");
+        for (char colonna : colonne) {
+            System.out.print("+-----");
+        }
+        System.out.println("+");
+        System.out.print(i + " ");
+        for (char colonna : colonne) {
+            System.out.print("|  ");
+            Pedina pedina = tabelloneStampato[i - 1][colonna - 'a'];
+            if (pedina != null) {
+                if (pedina.getCarattere() == 'A') {
+                    System.out.print(ANSI_YELLOW + pedina.getCarattere() + "\u001B[0m  ");
+                } else if (pedina.getCarattere() == 'B') {
+                    System.out.print(ANSI_ORANGE + pedina.getCarattere() + "\u001B[0m  ");
+                } else if (pedina.getCarattere() == 'C') {
+                    System.out.print("\u001B[35m" + pedina.getCarattere() + "\u001B[0m  "); // 'Z' in rosa
+                } else {
+                    System.out.print(pedina.getCarattere() + "  ");
+                }
+            } else {
+                System.out.print(".  ");
+            }
+        }
+        System.out.println("| " + i);
+    }
+
+    // Stampa la chiusura delle righe del tavoliere
+    System.out.print("  ");
+    for (char colonna : colonne) {
+        System.out.print("+-----");
+    }
+    System.out.println("+");
+
+    // Stampa l'intestazione delle colonne in fondo
+    System.out.print("    ");
+    for (char colonna : colonne) {
+        System.out.print("   " + colonna + "  ");
+    }
+    System.out.println();
+}
 }
 

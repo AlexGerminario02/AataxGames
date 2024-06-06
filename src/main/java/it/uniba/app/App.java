3d90c7021d2b1b66a3ae22a8c813b521d982a1f4
ditraniantonio <ditrani.antonio16@gmail.com>
2024-06-07 00:41:11 +0200

    modificato il file App.java

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,96 +1,89 @@
 package it.uniba.app;
 
+
 import it.uniba.app.Boundary.Costanti;
 import it.uniba.app.Boundary.Menu;
 import it.uniba.app.Boundary.Tastiera;
 import it.uniba.app.Control.Partita;
-import it.uniba.app.Entity.Coordinate;
 import it.uniba.app.Entity.Giocatore;
 import it.uniba.app.Entity.Pedina;
 import it.uniba.app.Entity.Tavoliere;
+import it.uniba.app.Entity.Coordinate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * <<Boundary>>
- * Main class of the application.
- * Questa classe rappresenta il punto di ingresso dell'applicazione Ataxx.
- *  
+ * Costruttore privato per evitare l'istanziazione della classe.
  */
 public final class App {
-    // Definizione delle sequenze di escape ANSI per i colori
 
     private App() {
-        // Costruttore privato per evitare l'instanziazione della classe
+        // Costruttore privato per evitare l'istanziazione della classe
     }
 
-    /**
-     * Metodo per stampare il benvenuto con i colori.
-     */
-    private static void stampaBenvenuto() {
-        System.out.println(Costanti.BENVENUTO_ASCII);
-        System.out.println(Costanti.INTRODUZIONE);
-        System.out.println(Costanti.REGOLE);
-    }
-
-    /**
-     * Entrypoint of the application.
-     *
-     * @param args command line arguments
-     */
+/**
+ * Metodo principale che avvia l'applicazione.
+ * Questo metodo gestisce il ciclo principale dell'applicazione,
+ * visualizza il menu e gestisce i comandi dell'utente.
+ *
+ * @param args Argomenti della riga di comando (non utilizzati).
+ */
     public static void main(final String[] args) {
-
         Tastiera tastiera = new Tastiera();
         Tavoliere tavoliere = Tavoliere.creaTavoliere();
-        Giocatore giocatore1 = new Giocatore(new Pedina('N', new Coordinate(0, 0)), "Nero");
-        Giocatore giocatore2 = new Giocatore(new Pedina('R', new Coordinate(0, 0)), "Rosso");
-        Partita partita = new Partita(giocatore1, giocatore2, tavoliere);
         boolean isRunning = true;
-
-        // Stampa il messaggio di benvenuto
+        Partita partita = null;
+        List<Coordinate> caselleDaBloccare = new ArrayList<>();
+        Menu.stampaBenvenuto();
 
         while (isRunning) {
-            stampaBenvenuto();
             System.out.println(Costanti.MENU_COMANDO_INIZIALE);
             String input = tastiera.readString(Costanti.PROMPT_COMANDO);
-
+                if (input.toLowerCase().startsWith("/blocca")) {
+                    Menu.clearScreen();
+                    Partita.gestioneBlocca(input, caselleDaBloccare, tavoliere);
+                    System.out.println();
+                    continue;
+            }
             switch (input.toLowerCase()) {
                 case "/help":
                 case "--help":
                 case "-h":
                     Menu.helpPrincipale(tastiera);
-                    Menu.delay(Costanti.TEMPO);
+                    Menu.delay(Costanti.TIME7);
                     Menu.clearScreen();
                     break;
-
                 case "/esci":
                     if (Menu.esci(tastiera)) {
                         isRunning = false;
                     }
                     break;
+
                 case "/gioca":
+                    Giocatore giocatore1 = new Giocatore(new Pedina('N', new Coordinate(0, 0)), "Nero");
+                    Giocatore giocatore2 = new Giocatore(new Pedina('R', new Coordinate(0, 0)), "Rosso");
+                    partita = new Partita(giocatore1, giocatore2, tavoliere);
                     Menu.clearScreen();
-                    partita.avviaPartita();
-                    if (partita.avviaPartita()) {
+                    if (partita.avviaPartita(caselleDaBloccare)) {
                         return;
                     } else {
-                        partita.reset();
+                        partita.reset(caselleDaBloccare);
                     }
                     break;
-                case "/vuoto":
+
+                    case "/vuoto":
                     Menu.clearScreen();
                     tavoliere.visualizzaTavoliereVuoto();
                     while (true) {
-                        String input2 = tastiera.readString(Costanti.COMANDO_HELP_GIOCO);
-                        if (input2.equalsIgnoreCase("/indietro")) {
+                            Menu.delay(Costanti.TIME5);
                             Menu.clearScreen();
-                            break; // Esce dal ciclo while e torna al menu principale
-                        } else {
-                            System.out.println(
-                        "Comando non riconosciuto. Per favore, digitare '/indietro' per tornare al menu principale.");
+                            break;
                         }
-                    }
-                    break;
+                        break;
                 default:
-                    // System.out.println("Scelta non valida. Premi un pulsante valido\n");
+                    System.out.println("Scelta non valida. Premi un pulsante valido\n");
+                    Menu.delay(2);
                     Menu.clearScreen();
             }
         }

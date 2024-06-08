7e38b14e1d801f003c4d4786b251cff9d4fcfe3f
pinopostino <cannonegiuseppe13@gmail.com>
2024-06-08 16:35:17 +0200

    Aggiunta la classe Mossa.java con relativa implementazione

---
+++ src/main/java/it/uniba/app/Entity/Mossa.java
@@ -0,0 +1,58 @@
+package it.uniba.app.Entity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * <<Entity>>: Classe per rappresentare le mosse possibili di una pedina.
+ */
+public class Mossa {
+    private static final int DIM = 7;
+    private final char[] colonne = {'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+    private Duplicazione mossea;
+    private Salto mosseb;
+    private Pedina[][] scacchiera;
+
+    /**
+     * Costruttore della classe Mossa.
+     * @param scacchieram La scacchiera su cui eseguire le mosse.
+     * @param mosseaa Le mosse di duplicazione associate.
+     * @param mossebb Le mosse di salto associate.
+     */
+    public Mossa(final Pedina[][] scacchieram, final Duplicazione mosseaa, final Salto mossebb) {
+        this.scacchiera = scacchieram;
+        this.mossea = mosseaa;
+        this.mosseb = mossebb;
+    }
+
+    /**
+     * Calcola le mosse possibili per la pedina.
+     * @param riga La riga della coordinata di partenza.
+     * @param colonna La colonna della coordinata di partenza.
+     * @return Un'ArrayList di Coordinate contenente le mosse possibili per la pedina.
+     */
+    public final ArrayList<Coordinate> mosseC(final int riga, final int colonna) {
+        ArrayList<Coordinate> mosseC = new ArrayList<>();
+        if (mossea != null) {
+            mosseC.addAll(mossea.mosseA(riga, colonna));
+        }
+        if (mosseb != null) {
+            mosseC.addAll(mosseb.mosseB(riga, colonna));
+        }
+        return mosseC;
+    }
+
+    /**
+     * Verifica se una posizione sulla scacchiera è vuota.
+     * @param riga La riga della posizione.
+     * @param colonna La colonna della posizione.
+     * @return true se la posizione è vuota, altrimenti false.
+     */
+    public final boolean posizioneVuota(final int riga, final char colonna) {
+        int indiceColonna = Arrays.binarySearch(colonne, Character.toLowerCase(colonna));
+        if (indiceColonna < 0 || riga < 1 || riga > DIM) {
+            return false;
+        }
+        return scacchiera[riga - 1][indiceColonna] == null;
+   }
+}

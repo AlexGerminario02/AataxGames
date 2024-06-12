7d82909777e678d973f42701caa3a8c6e55a5394
albertobracciolini <a.bracciolini04@tiscali.it>
2024-06-12 16:30:27 +0200

    Aggiunti casi di test per la classe Pedina.java

--- src/test/java/it/uniba/app/Entity/PedinaTest.java
+++ src/test/java/it/uniba/app/Entity/PedinaTest.java
@@ -1,6 +1,7 @@
 package it.uniba.app.Entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -78,5 +79,43 @@ import org.junit.jupiter.api.Test;
         pedina1.setCoordinate(coord2);
         assertEquals(coord2, pedina1.getCoordinate(), "Coordinates should be"
         + "(2, 2) after setCoordinate");  // Verifica che il setCoordinate cambia le coordinate correttamente
+    }
+
+    /**
+     * Test per il metodo equals.
+     */
+    @Test
+    void testEquals() {
+        Pedina samePedina = new Pedina("X", coord1);
+        assertEquals(pedina1, samePedina, "Pedina1 should be equal to"
+        + "another Pedina with the same attributes");  // Verifica che due pedine con gli stessi attributi sono uguali
+    }
+
+    /**
+     * Test per il metodo equals con oggetti diversi.
+     */
+    @Test
+    void testNotEquals() {
+        assertNotEquals(pedina1, pedina2, "Pedina1 should not be equal"
+       + " to Pedina2 with different attributes");  // Verifica che due pedine con attributi diversi non sono uguali
+    }
+
+    /**
+     * Test per il metodo hashCode.
+     */
+    @Test
+    void testHashCode() {
+        Pedina samePedina = new Pedina("X", coord1);
+        assertEquals(pedina1.hashCode(), samePedina.hashCode(), "HashCode should be"
+        + "the same for Pedina with same attributes");  // Verifica che il hashCode sia lo stesso per oggetti uguali
+    }
+
+    /**
+     * Test per il metodo toString.
+     */
+    @Test
+    void testToString() {
+        String expected = "Pedina{carattere=X, coordinate=(b  1 )}";
+        assertEquals(expected, pedina1.toString(), "toString should return the expected string representation");
     }
 }
